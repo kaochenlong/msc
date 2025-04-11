c56958992fcf96ca6fc699dc8a120a9cb087e38a
Eddie Kao <eddie@5xcampus.com>
2025-04-11 16:12:38 +0800

    init commit

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def home(req):
+    return render(req, "home.html")
+
+
+def about(req):
+    return render(req, "about.html")
+
+
+def contact(req):
+    return render(req, "contact.html")
