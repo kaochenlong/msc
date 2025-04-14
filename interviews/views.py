91867efcf674b1df3b1c301da61eb1d32d7c8942
Eddie Kao <eddie@5xcampus.com>
2025-04-14 16:54:18 +0800

    feat: add show page

--- interviews/views.py
+++ interviews/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from .models import Interview
 
 
 def index(req):
-    return render(req, "interviews/index.html")
+    interviews = Interview.objects.all()
+    return render(
+        req,
+        "interviews/index.html",
+        {"interviews": interviews},
+    )
+
+
+def show(req, id):
+    # pk = primary key
+    interview = Interview.objects.get(pk=id)
+    return render(
+        req,
+        "interviews/show.html",
+        {"interview": interview},
+    )
 
 
 def new(req):
