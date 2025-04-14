9a007690c2f6cb8b0d4b31d3ccbe7d91215da9a9
Eddie Kao <eddie@5xcampus.com>
2025-04-14 12:26:23 +0800

    feat: add interview pages

--- interviews/urls.py
+++ interviews/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "interviews"
+
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
+    path("new", views.new, name="new"),
 ]
--- interviews/views.py
+++ interviews/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(req):
     return render(req, "interviews/index.html")
+
+
+def new(req):
+    return render(req, "interviews/new.html")
