9954dda2c9a7de0860ff8ec55b977fd6ab0b82d3
Eddie Kao <eddie@5xcampus.com>
2025-04-22 17:28:55 +0800

    feat: use decorator

--- users/views.py
+++ users/views.py
@@ -1,35 +1,36 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.views.decorators.http import require_POST
 
 
+@require_POST
 def index(req):
-    if req.POST:
-        form = UserCreationForm(req.POST)
-        form.save()
+    form = UserCreationForm(req.POST)
+    form.save()
 
-        return redirect("pages:home")
+    return redirect("pages:home")
 
 
 def sign_in(req):
     return render(req, "users/sign_in.html")
 
 
+@require_POST
 def create_session(req):
-    if req.POST:
-        username = req.POST["username"]
-        password = req.POST["password"]
+    username = req.POST["username"]
+    password = req.POST["password"]
 
-        user = authenticate(
-            username=username,
-            password=password,
-        )
+    user = authenticate(
+        username=username,
+        password=password,
+    )
 
-        if user is not None:
-            login(req, user)
-            return redirect("pages:home")
-        else:
-            return redirect("users:sign_in")
+    if user is not None:
+        login(req, user)
+        return redirect("pages:home")
+    else:
+        return redirect("users:sign_in")
 
 
 def sign_up(req):
